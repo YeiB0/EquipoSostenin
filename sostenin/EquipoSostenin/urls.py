db613fd3bf294db16fd077c5efb3384b04d917fe
Ni1c0o <236280637+Ni1c0o@users.noreply.github.com>
2025-11-15 18:50:49 -0300

    Se arreglo la duplicacion de archivos, ordenando un poco las carpetas del proyecto

--- sostenin/EquipoSostenin/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home_view, name='home'),
-    path('subir/', views.subir_boleta_view, name='subir_boleta'),
-    path('dashboard/', views.dashboard_view, name='dashboard'),
-    path('boleta/<int:boleta_id>/delete/', views.delete_boleta_view, name='delete_boleta'),
-]
